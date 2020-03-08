c516b5c5c2dccd3a70ffc4d781e33a18c2ba25ef
kororo11 <kororo11@naver.com>
2020-03-08 22:15:15 +0900

    Create tamplate.js
    템플릿 리터럴 연습

---
+++ tamplate.js
@@ -0,0 +1,8 @@
+const a = 20;
+const b = 8;
+const c = '자바스크립트';
+const str1 = '저는 ' + (a + b) + '살이고 ' + c + '를 좋아합니다.';
+console.log(str1);
+
+const str2 = `저는 ${a + b} 살이고 ${c}를 좋아합니다.`;
+console.log(str2);
