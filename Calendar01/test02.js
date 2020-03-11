16b2657635fb8e0dc8e16ff83a2486c2f5b16fcf
kororo11 <kororo11@naver.com>
2020-03-11 18:36:59 +0900

    Create test02.js
    그냥 날짜를 넣으면 뭐가 나올까해서 해봤는데, vscode 에서 나오는 결과와 크롬에서 나오는 결과가 다르다

---
+++ Calendar01/test02.js
@@ -0,0 +1,18 @@
+// var today = new Date();
+// var dd = today.getDate();
+// var mm = today.getMonth() + 1; //January is 0!
+// var yyyy = today.getFullYear();
+
+// if (dd < 10) {
+//   dd = '0' + dd;
+// }
+
+// if (mm < 10) {
+//   mm = '0' + mm;
+// }
+
+// today = mm + '/' + dd + '/' + yyyy;
+// console.log(today);
+
+var d = new Date(2020, 3 - 1, 1);
+console.log('d', d);
