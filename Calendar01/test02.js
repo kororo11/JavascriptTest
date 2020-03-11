9df241141dc72801a437d0c8d7bf2bd0e0f381f4
kororo11 <kororo11@naver.com>
2020-03-11 22:17:33 +0900

    ㅁㅁ
    별 내용 없어요

--- Calendar01/test02.js
+++ Calendar01/test02.js
@@ -14,5 +14,49 @@
 // today = mm + '/' + dd + '/' + yyyy;
 // console.log(today);
 
-var d = new Date(2020, 3 - 1, 1);
-console.log('d', d);
+// var d = new Date(2020, 3 - 1, 1);
+// console.log('d', d);
+
+// new Date();
+// new Date(value);
+// new Date(dateString);
+// new Date(year, monthIndex[, day[, hour[, minutes[, seconds[, milliseconds]]]]]);
+
+// monthIndex : 월을 나타내는 정숫값. 0은 1월을 나타내고 11은 12월을 나타냅니다.
+// day : 일을 나타내는 정숫값. 기본값은 1입니다.
+
+// 참고 링크
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+// 1월-January (제뉴어리)
+
+// 2월-February (페뷰어리)
+
+// 3월-March (머르츠)
+
+// 4월-April (에이프릴)
+
+// 5월-May (메이)
+
+// 6월-June (준)
+
+// 7월-July (줄라이)
+
+// 8월-August (어거스트)
+
+// 9월-September (셉템버)
+
+// 10월-October (옥토버)
+
+// 11월-November (노벰버)
+
+// 12월-December (디셈버)
+
+// var January = new Date(2020, 0, 1);
+// console.log('January -> ', January);
+
+// var February = new Date(2020, 1, 1);
+// console.log('February -> ', February);
+
+var April = new Date(2020, 3, 1);
+console.log('January -> ', April);
