ff16bb097f6b39cb0e5c99d1e266c12d412e5311
kororo11 <kororo11@naver.com>
2020-03-18 13:58:56 +0900

    함수 안에서 접근 가능한 변수명과 동일한 이름을 가진 ㅕㄴ수를 선언하면, 사용한 키워드에 따라 가리키는 대상이 달라진다.

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
-var i = 0;
-while (i < 10) {
-    i++;
-    if (i % 2 === 0) {
-        continue;
-    }
-    alert(i);
+// var i = 0;
+// while (i < 10) {
+//     i++;
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     alert(i);
+// }
+
+var a = 1;
+let b = 2;
+
+function myFunction() {
+  var a = 3; // 전혀 다른 변수
+  let b = 4; // 전혀 다른 변수
+
+  if (true) {
+    var a = 5; // 덮어쓴다.
+    let b = 6; // 전혀 다른 변수
+
+    console.log(a);
+    console.log(b);
+  }
+
+  console.log(a);
+  console.log(b);
 }
+
+myFunction();
+
+console.log(a);
+console.log(b);
